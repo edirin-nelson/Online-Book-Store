6cca157f9e7954f7d261caffd574412ef14d6640
edirin-nelson <nelsonedirin@gmail.com>
2025-02-05 16:51:21 +0100

    initial commit

---
+++ src/main/java/com/interswitch/InterswitchAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.interswitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterswitchAssessmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterswitchAssessmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/interswitch/InterswitchAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interswitch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterswitchAssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
